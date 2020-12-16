55cd58e5feeaac4f226c6ef9f06ef323bc7f91d2
echandle2228 <echandle@cord.edu>
2020-12-16 10:29:21 -0600

    Added front and tremp to the randomization

--- programs/1_greenhouse_design.R
+++ programs/1_greenhouse_design.R
@@ -3,45 +3,49 @@
 remove(list=ls())
 set.seed(654841)
 
-#' Random selection for Southern plant included in study
-(Southern <- sample(x = 1:26, size = 20, replace = F))
+#' Random selection for Prairie Island plants included in study
+(PI <- sample(x = 1:19, size = 9, replace = F))
+#' Random selection for Tremp plants included in study
+(TR <- sample(x = 1:30, size = 9, replace = F))
+#' Random selection for front plants included in study
+(FR <- sample(x = 1:13, size = 8, replace = F))
 
 #' Randomization for ramet A for all plants
-(A_north <- sample(x = 1:20, size = 20, replace = F))
-(A_south <- sample(x = 1:20, size = 20, replace = F))
+(A_north <- sample(x = 1:26, size = 26, replace = F))
+(A_south <- sample(x = 1:26, size = 26, replace = F))
 
 #' Coin flip for which position(left or right) to place northern plant
-A_placement <- sample(x = c("left", "right"), size = 20, replace = T)
+A_placement <- sample(x = c("left", "right"), size = 26, replace = T)
 
 A_ramet <- data.frame(A_north, A_south, A_placement)
 
 
 #' Randomization for ramet B for all plants
-(B_north <- sample(x = 1:20, size = 20, replace = F))
-(B_south <- sample(x = 1:20, size = 20, replace = F))
+(B_north <- sample(x = 1:26, size = 26, replace = F))
+(B_south <- sample(x = 1:26, size = 26, replace = F))
 
 #' Coin flip for which position(left or right) to place northern plant
-B_placement <- sample(x = c("left", "right"), size = 20, replace = T)
+B_placement <- sample(x = c("left", "right"), size = 26, replace = T)
 
 B_ramet <- data.frame(B_north, B_south, B_placement)
 
 
 #' Randomization for ramet C for all plants
-(C_north <- sample(x = 1:20, size = 20, replace = F))
-(C_south <- sample(x = 1:20, size = 20, replace = F))
+(C_north <- sample(x = 1:26, size = 26, replace = F))
+(C_south <- sample(x = 1:26, size = 26, replace = F))
 
 #' Coin flip for which position(left or right) to place northern plant
-C_placement <- sample(x = c("left", "right"), size = 20, replace = T)
+C_placement <- sample(x = c("left", "right"), size = 26, replace = T)
 
 C_ramet <- data.frame(C_north, C_south, C_placement)
 
 
 #' Randomization for ramet D for all plants
-(D_north <- sample(x = 1:20, size = 20, replace = F))
-(D_south <- sample(x = 1:20, size = 20, replace = F))
+(D_north <- sample(x = 1:26, size = 26, replace = F))
+(D_south <- sample(x = 1:26, size = 26, replace = F))
 
 #' Coin flip for which position(left or right) to place northern plant
-D_placement <- sample(x = c("left", "right"), size = 20, replace = T)
+D_placement <- sample(x = c("left", "right"), size = 26, replace = T)
 
 D_ramet <- data.frame(D_north, D_south, D_placement)
 
